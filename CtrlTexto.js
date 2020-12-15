7d57468507ff5e3492277e509da4c084fd6a4b54
afigueroa132 <75814448+afigueroa132@users.noreply.github.com>
2020-12-14 22:56:33 -0600

    Update CtrlTexto.js

--- CtrlTexto.js
+++ CtrlTexto.js
@@ -6,7 +6,7 @@ var forma = document.getElementById("forma"),
 forma.addEventListener("submit", procesa, false);
 function procesa() {
     for (var i = 0, longitud = campos.length; i < longitud; i++) {
-        var campo = forma[campos[i]];
+        var campo = forma(campos[i]);
         var salida = document.getElementById(salidas[i]);
         salida.value = campo.value;
     }
